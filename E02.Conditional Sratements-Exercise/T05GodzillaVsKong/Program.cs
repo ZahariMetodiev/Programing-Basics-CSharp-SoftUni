7caac07afc02f6c056a5b0c6ef427ef013fa760a
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-07 09:11:38 +0300

    Add task GodzillaVsKong

---
+++ E02.Conditional Sratements-Exercise/T05GodzillaVsKong/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace T05GodzillaVsKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double filmBudget = double.Parse(Console.ReadLine());
+            int filmPerson = int.Parse(Console.ReadLine());
+            double cloutesPrice = double.Parse(Console.ReadLine());
+
+            double decor = filmBudget * 0.10;
+            double totalCloutesPrice = cloutesPrice * filmPerson;
+
+            if (filmPerson > 150)
+            {
+                totalCloutesPrice = totalCloutesPrice -  (totalCloutesPrice * 0.1);
+            }
+
+            double totalSum = totalCloutesPrice + decor;
+
+            if (totalSum > filmBudget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {Math.Abs(filmBudget - totalSum):F2} leva more.");
+            }
+            else if (totalSum <= filmBudget)
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {filmBudget - totalSum:F2} leva left.");
+            }
+        }
+    }
+}
