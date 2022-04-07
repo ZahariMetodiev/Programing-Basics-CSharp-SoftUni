25134761a3b44da4ad42e11764f030612c44d777
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-07 09:08:50 +0300

    Add task SumSeconds

---
+++ E02.Conditional Sratements-Exercise/T01SumSeconds/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T01SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secontTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int totalTime = firstTime + secontTime + thirdTime;
+            int minutes = totalTime / 60;
+            int secont = totalTime % 60;
+
+            if (secont < 10)
+            {
+                Console.WriteLine($"{minutes}:0{secont}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{secont}");
+            }
+        }
+    }
+}
