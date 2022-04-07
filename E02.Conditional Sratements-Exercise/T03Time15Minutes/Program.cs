1a6c65f57c38915844c8016104322fcf79455eca
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-07 09:10:20 +0300

    Add task Time15Minutes

---
+++ E02.Conditional Sratements-Exercise/T03Time15Minutes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace T03Time15Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutes = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
