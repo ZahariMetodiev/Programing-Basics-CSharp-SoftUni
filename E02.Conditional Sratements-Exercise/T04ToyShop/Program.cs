aa83f0c79172fd35dd38ee6c24c2dc2da5074b1c
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-07 09:11:05 +0300

    Add task ToyShop

---
+++ E02.Conditional Sratements-Exercise/T04ToyShop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace T04ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double excursionPrice = double.Parse(Console.ReadLine());
+            
+            int puzzles = int.Parse(Console.ReadLine());
+            int doll = int.Parse(Console.ReadLine());
+            int bears = int.Parse(Console.ReadLine());
+            int minions = int.Parse(Console.ReadLine());
+            int trucks = int.Parse(Console.ReadLine());
+
+            double toysPrice = puzzles * 2.60 + doll * 3 + bears * 4.10 + minions * 8.20 + trucks * 2;
+            int toysNumber = puzzles + doll + bears + minions + trucks;
+
+            if (toysNumber >= 50)
+            {
+                toysPrice = toysPrice - (toysPrice * 0.25);
+            }
+
+            double totalPrice = toysPrice - (toysPrice * 0.10);
+
+            if (totalPrice >= excursionPrice)
+            {
+                Console.WriteLine($"Yes! {totalPrice - excursionPrice:F2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {Math.Abs(excursionPrice - totalPrice):F2} lv needed.");
+            }
+
+        }
+    }
+}
