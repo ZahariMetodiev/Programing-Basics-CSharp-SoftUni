8bb9323923aaae04c93e148a14dc824ca9940130
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-07 09:12:18 +0300

    Add task WorldSwimmingRecord

---
+++ E02.Conditional Sratements-Exercise/T06WorldSwimmingRecord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T06WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timeForOneMeter = double.Parse(Console.ReadLine());
+
+            double time = distance * timeForOneMeter;
+            double totalTime = Math.Round(distance / 15) * 12.5 + time;
+
+            if (totalTime < record)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:F2} seconds.");
+            }
+            else if (totalTime >= record)
+            {
+                Console.WriteLine($"No, he failed! He was {totalTime - record:F2} seconds slower.");
+            }
+        }
+    }
+}
