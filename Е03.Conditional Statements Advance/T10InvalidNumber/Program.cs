2877b8465fbbbecc163a8ff436425046cc24372e
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-08 09:46:09 +0300

    Add task InvalidNumber

---
+++ Е03.Conditional Statements Advance/T10InvalidNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T10InvalidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (!(num >= 100 && num <= 200 || num == 0))
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
