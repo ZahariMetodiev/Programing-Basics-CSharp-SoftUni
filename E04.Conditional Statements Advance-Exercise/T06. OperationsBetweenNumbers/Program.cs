7fcdee26d20503953971f6ce2fa8b882306cd1c4
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-09 10:28:42 +0300

    Add task OperationBetweenNumbers

---
+++ E04.Conditional Statements Advance-Exercise/T06. OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OperationBetweenNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+            string oddEven = "odd";
+            string output = "";
+            double sum = 0;
+
+            if (symbol == '+' || symbol == '-' || symbol == '*')
+            {
+                if (symbol == '+')
+                {
+                    sum = num1 + num2;
+                }
+                else if (symbol == '-')
+                {
+                    sum = num1 - num2;
+                }
+                if (symbol == '*')
+                {
+                    sum = num1 * num2;
+                }
+
+                if (sum % 2 == 0)
+                {
+                    oddEven = "even";
+                }
+                Console.WriteLine($"{num1} {symbol} {num2} = {sum} - {oddEven}");
+            }
+
+            else if (num2 == 0)
+            {
+                Console.WriteLine($"Cannot divide {num1} by zero");
+            }
+
+            else if (symbol == '/')
+            {
+                sum = num1 / num2;
+                Console.WriteLine($"{num1} {symbol} {num2} = {sum:f2}");
+            }
+
+            else if (symbol == '%')
+            {
+                sum = num1 % num2;
+                Console.WriteLine($"{num1} {symbol} {num2} = {sum}");
+            }
+        }
+    }
+}
