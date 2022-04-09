28505e67201defb78ec81dd27516494a961f13da
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-09 10:27:01 +0300

    Add task Cinema

---
+++ E04.Conditional Statements Advance-Exercise/T01. Cinema/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01._
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PREMIERE_PRICE = 12.00;
+            const double NORMAL_PRICE = 7.50;
+            const double DISCOUNT_PRICE = 5.00;
+
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+
+            int totalPlaces = rows * colums;
+            double income = 0;
+
+            switch (type)
+            {
+                case "Premiere":
+                    income = totalPlaces * PREMIERE_PRICE;
+                    break;
+                case "Normal":
+                    income = totalPlaces * NORMAL_PRICE;
+                    break;
+                case "Discount":
+                    income = totalPlaces * DISCOUNT_PRICE;
+                    break;
+            }
+
+            Console.WriteLine($"{income:f2} leva");
+        }
+    }
+}
