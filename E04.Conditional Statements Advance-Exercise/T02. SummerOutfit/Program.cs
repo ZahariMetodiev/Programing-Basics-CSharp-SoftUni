4b7921702ddf88c9548f8d556b9e522d9b351d82
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-09 10:27:23 +0300

    Add task SummerOutfit

---
+++ E04.Conditional Statements Advance-Exercise/T02. SummerOutfit/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace programingBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degrees = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            string outfit = "";
+            string shoes = "";
+
+            if (day == "Morning")
+            {
+                if (degrees >= 10 && degrees <= 18)
+                {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                else if (degrees <= 24)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+            }
+            else if (day == "Afternoon")
+            {
+                if (degrees >= 10 && degrees <= 18)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (degrees <= 24)
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+            }
+            else if (day == "Evening")
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
