e425a535b13913542a90090fc4f8e9075ba1df94
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-10 09:35:28 +0300

    Add task SumNumbers

---
+++ E05.For Loop/T07SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T07SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                int number =int.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
