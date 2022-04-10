220a21fa5e8249dad3558e20bae5520e7389bdb8
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-10 09:30:32 +0300

    Add tasks

---
+++ E05.For Loop/Т01Numbersfroм1to100/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Т01Numbersfroм1to100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
