8ad546ff6408ab9896f42a32cef7005a444f7b16
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-10 09:36:37 +0300

    Add task OddEvenSum

---
+++ E05.For Loop/T10. Odd Even Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _10._Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum += currentNum;
+                }
+                else
+                {
+                    oddSum += currentNum;
+                }
+            }
+            int different = Math.Abs(evenSum - oddSum);
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {different}");
+            }
+        }
+    }
+}
