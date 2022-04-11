0a49864e08605815f388320cd51087e6631d0b0b
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-11 09:51:28 +0300

    Add task Oscars

---
+++ T06.For Loop-Exercise/T06. Oscars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace T06._Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfActor = Console.ReadLine();
+            double pointsFromAcademy = double.Parse(Console.ReadLine());
+            int numberOfJury = int.Parse(Console.ReadLine());
+
+            double sumPoints = pointsFromAcademy;
+
+            for (int i = 1; i <= numberOfJury; i++)
+            {
+                string nameOfJury = Console.ReadLine();
+                double pointsFromJury = double.Parse(Console.ReadLine());
+                double momentPoints =(nameOfJury.Length * pointsFromJury) / 2;
+                sumPoints += momentPoints;
+
+                if (pointsFromAcademy > 1250.50)
+                {
+                    Console.WriteLine($"Congratulations, {nameOfActor} got a nominee for leading role with {sumPoints:f1}!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry, {nameOfActor} you need {1250.5 - sumPoints:f1} more");
+                }
+            }
+        }
+    }
+}
