77f9cf76d93f753dbaa525f59e5286a070b7cc69
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-11 09:49:34 +0300

    Add task HalfSumElements

---
+++ T06.For Loop-Exercise/T02. Half Sum Element/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace T02._Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int maxNum = int.MinValue;
+
+            for (int i = 0; i < num; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+                if (currentNum > maxNum)
+                {
+                    maxNum = currentNum;
+                }
+            }
+            int totalSum = sum - maxNum;
+            if (totalSum == maxNum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNum}");
+            }
+            else
+            {
+                int diff = Math.Abs(totalSum - maxNum);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
