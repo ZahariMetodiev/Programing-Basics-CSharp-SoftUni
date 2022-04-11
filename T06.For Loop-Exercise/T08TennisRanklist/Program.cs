0d2e52b74bb20b4e8de34872b3e2cbdf4d8a1af8
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-11 09:52:39 +0300

    Add task TennisRanklist

---
+++ T06.For Loop-Exercise/T08TennisRanklist/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace T08TennisRanklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int W_POINT = 2000;
+            const int F_POINT = 1200;
+            const int SF_POINT = 720;
+
+            int numberOfTournament = int.Parse(Console.ReadLine());
+            int initialPoint = int.Parse(Console.ReadLine());
+
+            int finalPoint = initialPoint;
+            int wins = 0;
+
+            for (int i = 0; i < numberOfTournament; i++)
+            {
+                string reachedStage = Console.ReadLine();
+
+                if (reachedStage == "W")
+                {
+                    finalPoint += W_POINT;
+                    wins++;
+                }
+                else if (reachedStage == "F")
+                {
+                    finalPoint += F_POINT;
+                }
+                else if (reachedStage == "SF")
+                {
+                    finalPoint += SF_POINT;
+                }
+            }
+
+            double averagePoint = (double)(finalPoint - initialPoint) / numberOfTournament;
+            double percentWins = (double)wins / numberOfTournament * 100;
+
+            Console.WriteLine($"Final points: {finalPoint}");
+            Console.WriteLine($"Average points: {Math.Floor(averagePoint)}");
+            Console.WriteLine($"{percentWins:f2}%");
+
+        }
+    }
+}
