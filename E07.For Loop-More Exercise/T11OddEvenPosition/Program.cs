58c25b57edc9e6ad280bc172dbb5a6489744f83f
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-12 10:18:44 +0300

    Add task OddEvenPosition

---
+++ E07.For Loop-More Exercise/T11OddEvenPosition/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace T11OddEvenPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            double oddSum = 0;
+            double oddMin = double.MaxValue;
+            double oddMax = double.MinValue;
+            double evenSum = 0;
+            double evenMin = double.MaxValue;
+            double evenMax = double.MinValue;
+
+            for (int i = 1; i <= numbers; i++)
+            {
+                double num = double.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    evenSum += num;
+                    if (num > evenMax)
+                    {
+                        evenMax = num;
+                    }
+                    if (num < evenMin)
+                    {
+                        evenMin = num;
+                    }
+                }
+                else
+                {
+                    oddSum += num;
+                    if (num > oddMax)
+                    {
+                        oddMax = num;
+                    }
+                    if (num < oddMin)
+                    {
+                        oddMin = num;
+                    }
+                }
+            }
+            Console.WriteLine($"OddSum={oddSum:f2},");
+            if (oddMin != double.MaxValue)
+            {
+                Console.WriteLine($"OddMin={oddMin:f2},");
+            }
+            else
+            {
+                Console.WriteLine($"OddMin=No,");
+            }
+            if (oddMax != double.MinValue)
+            {
+                Console.WriteLine($"OddMax={oddMax:f2},");
+            }
+            else
+            {
+                Console.WriteLine($"OddMax=No,");
+            }
+            Console.WriteLine($"EvenSum={evenSum:f2},");
+            if (evenMin != double.MaxValue)
+            {
+                Console.WriteLine($"EvenMin={evenMin:f2},");
+            }
+            else
+            {
+                Console.WriteLine($"EvenMin=No,");
+            }
+            if (evenMax != double.MinValue)
+            {
+                Console.WriteLine($"EvenMax={evenMax:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"EvenMax=No");
+            }
+        }
+    }
+}
