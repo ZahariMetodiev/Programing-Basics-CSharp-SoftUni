676d29786417ee056ba0fe52db8e654e3060bcd1
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-12 10:18:19 +0300

    Add task ClockPart2

---
+++ E07.For Loop-More Exercise/T10ClockPart2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T10ClockPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour <= 23; hour++)
+            {
+                for (int minute = 0; minute <= 59; minute++)
+                {
+                    for (int second = 0; second <= 59; second++)
+                    {
+                        Console.WriteLine($"{hour} : {minute} : {second}");
+                    }
+                }
+            }
+        }
+    }
+}
