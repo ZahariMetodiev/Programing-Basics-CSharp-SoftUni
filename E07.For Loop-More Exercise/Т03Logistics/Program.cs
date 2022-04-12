bd921a1355104a80e1f5b0087c33053c0cf59c0c
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-12 10:19:01 +0300

    Add task Logistics

---
+++ E07.For Loop-More Exercise/Т03Logistics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Т03Logistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loads = int.Parse(Console.ReadLine());
+
+            int minibusCount = 0;
+            int truckCount = 0;
+            int trainCount = 0;
+            int totalCount = 0;
+
+            for (int i = 1; i <= loads; i++)
+            {
+                int weight = int.Parse(Console.ReadLine());
+
+                totalCount += weight;
+
+                if (weight <= 3)
+                {
+                    minibusCount += weight;
+                }
+                else if (weight <= 11)
+                {
+                    truckCount += weight;
+                }
+                else
+                {
+                    trainCount += weight;
+                }
+            }
+
+            double averagePrice = (double)(minibusCount * 200 + truckCount * 175 + trainCount * 120) / totalCount;
+            double minibusPercent = (double)minibusCount / totalCount * 100;
+            double truckPercent = (double)truckCount / totalCount * 100;
+            double trainPercent = (double)trainCount / totalCount * 100;
+
+            Console.WriteLine($"{averagePrice:f2}");
+            Console.WriteLine($"{minibusPercent:f2}%");
+            Console.WriteLine($"{truckPercent:f2}%");
+            Console.WriteLine($"{trainPercent:f2}%");
+        }
+    }
+}
