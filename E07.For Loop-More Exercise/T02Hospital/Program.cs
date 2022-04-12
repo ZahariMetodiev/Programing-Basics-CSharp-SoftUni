7666c03cba1ae2d69e78a1776cdbac47e93cc9b4
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-12 10:15:22 +0300

    Add task Hospital

---
+++ E07.For Loop-More Exercise/T02Hospital/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace T02Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = int.Parse(Console.ReadLine());
+
+            int untreatedPatients = 0;
+            int treatedPatients = 0;
+
+            for (int day = 1; day <= period; day++)
+            {
+                int patients = int.Parse(Console.ReadLine());
+
+                if (day % 3 == 0 && untreatedPatients > treatedPatients)
+                {
+                    if (patients <= 8 )
+                    {
+                        treatedPatients += patients;
+                    }
+                    else
+                    {
+                        treatedPatients += 8;
+                        untreatedPatients += patients - 8;
+                    }
+                }
+                else
+                {
+                    if (patients <= 7)
+                    {
+                        treatedPatients += patients;
+                    }
+                    else
+                    {
+                        treatedPatients += 7;
+                        untreatedPatients += patients - 7;
+                    }
+                }
+            }
+            Console.WriteLine($"Treated patients: {treatedPatients}.");
+            Console.WriteLine($"Untreated patients: {untreatedPatients}.");
+        }
+    }
+}
