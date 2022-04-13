f25c86050a7ca9256fc4014738ff3f9020711117
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-13 08:28:11 +0300

    Add task MaxNumber

---
+++ E08.While Loop/T06. MaxNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace T06._Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int maxNum = int.MinValue;
+
+            while (input != "Stop")
+            {
+                int currentNum = int.Parse(input);
+                if (currentNum > maxNum)
+                {
+                    maxNum = currentNum;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
