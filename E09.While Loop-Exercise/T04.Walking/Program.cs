c45c5c85020ad7c29f0cf43804e0d33a83a6d534
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-14 09:23:36 +0300

    Add task Walking

---
+++ E09.While Loop-Exercise/T04.Walking/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace T04._Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string steps = Console.ReadLine();
+            int stepsSum = 0;
+
+            while (stepsSum < 10000)
+            {
+
+                if (steps == "Going home")
+                {
+                    int stepsToHome = int.Parse(Console.ReadLine());
+                    stepsSum += stepsToHome;
+                    break;
+                }
+                int currentSteps = int.Parse(steps);
+                stepsSum += currentSteps;
+                if (stepsSum >=10000)
+                {
+                    break;
+                }
+                steps = Console.ReadLine();
+            }
+            if (stepsSum >= 10000)
+            {
+                Console.WriteLine($"Goal reached! Good job!");
+                Console.WriteLine($"{stepsSum - 10000} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{10000 - stepsSum} more steps to reach goal.");
+            }
+        }
+    }
+}
