ce1842bebc917c75d31bb76771a90dd0b959b85b
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-14 09:21:31 +0300

    Add task OldBooks

---
+++ E09.While Loop-Exercise/T01.OldBooks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T01._Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favorityBook = Console.ReadLine();
+
+            int counter = 0;
+            string nextBook = Console.ReadLine();
+
+            while (nextBook != "No More Books")
+            {
+                if (nextBook == favorityBook)
+                {
+                    Console.WriteLine($"You checked {counter} books and found it.");
+                    return;
+                }
+                counter++;
+                nextBook = Console.ReadLine();
+            }
+            Console.WriteLine($"The book you search is not here!");
+            Console.WriteLine($"You checked {counter} books.");
+        }
+    }
+}
