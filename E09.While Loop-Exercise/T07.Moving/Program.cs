eb0fbcc63ad93a852c9cbbee0d3386e9b3c11b99
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-14 09:25:12 +0300

    Add task Moving

---
+++ E09.While Loop-Exercise/T07.Moving/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace T07._Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int roomWidth = int.Parse(Console.ReadLine());
+            int roomLenght = int.Parse(Console.ReadLine());
+            int roomHeight = int.Parse(Console.ReadLine());
+
+            int area = roomWidth * roomLenght * roomHeight;
+            int currentArea = 0;
+
+            string boxes = Console.ReadLine();
+
+            while (boxes != "Done")
+            {
+                int currentBoxes = int.Parse(boxes);
+                currentArea += currentBoxes;
+                if (currentArea >= area)
+                {
+                    Console.WriteLine($"No more free space! You need {currentArea - area} Cubic meters more.");
+                    return;
+                }
+
+                boxes = Console.ReadLine();
+            }
+            Console.WriteLine($"{area - currentArea} Cubic meters left.");
+        }
+    }
+}
