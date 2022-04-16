488262d34b01a68ca59ab9b96156050d133c383d
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-16 11:32:20 +0300

    Add task AccountBalance

---
+++ E08.While Loop/T05.AccountBalance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T05.AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double money = 0;
+
+            while (input != "NoMoreMoney")
+            {
+                double currentMoney = double.Parse(input);
+                if (currentMoney < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                money += currentMoney;
+                Console.WriteLine($"Increase: {currentMoney:f2}");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total: {money:f2}");
+        }
+    }
+}
