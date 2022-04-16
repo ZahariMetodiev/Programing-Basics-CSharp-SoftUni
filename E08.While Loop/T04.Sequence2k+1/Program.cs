0ad0ab046df934019e3f9dfae37a0683d57cb238
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-16 11:31:54 +0300

    Add task Sequence

---
+++ E08.While Loop/T04.Sequence2k+1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T04.Sequence2k1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int i = 1;
+
+            while (i <= num)
+            {
+                Console.WriteLine(i);
+                i = i * 2 + 1;
+            }
+        }
+    }
+}
