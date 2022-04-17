ef57b05dee95a524ef6eb4af73a96449430f04c4
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-17 10:35:22 +0300

    Add task AverageNumber

---
+++ E10.While Loop-More Exercise/T05AverageNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T05AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            for (int i = 0; i < numbers; i++)
+            {
+                double num = double.Parse(Console.ReadLine());
+                sum += num;
+            }
+            double averageNum = sum / numbers;
+            Console.WriteLine($"{averageNum:f2}");
+        }
+    }
+}
