37fd867b70cf400f41f50a1726ad0d4ebfc5adad
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-18 09:57:46 +0300

    Add task BraceletStand

---
+++ E13.Exam Practice/T01.Exam/T02BraceletStand/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T02BraceletStand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pocketMoney = double.Parse(Console.ReadLine());
+            double earnedMoney = double.Parse(Console.ReadLine());
+            double expence = double.Parse(Console.ReadLine());
+            double giftPrice = double.Parse(Console.ReadLine());
+
+            double totalSum = (5 * pocketMoney) + (5 * earnedMoney) - expence;
+
+            if (totalSum >= giftPrice)
+            {
+                Console.WriteLine($"Profit: {totalSum:f2} BGN, the gift has been purchased.");
+            }
+            else
+            {
+                Console.WriteLine($"Insufficient money: {giftPrice - totalSum:f2} BGN.");
+            }
+        }
+    }
+}
