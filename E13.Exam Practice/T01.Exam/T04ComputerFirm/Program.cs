c63c025bbe3f79f504bc53071db04a2639586914
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-18 09:58:44 +0300

    Add task ComputerFirm

---
+++ E13.Exam Practice/T01.Exam/T04ComputerFirm/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace T04ComputerFirm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfComputers = int.Parse(Console.ReadLine());
+            int totalRaiting = 0;
+            double realisedSales = 0;
+
+            for (int i = 0; i < numberOfComputers; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                int raiting = num % 10;
+                totalRaiting += raiting;
+                int sales = num / 10;
+
+                if (raiting == 2)
+                {
+                    realisedSales += 0;
+                }
+                else if (raiting == 3)
+                {
+                    realisedSales += sales / 2;
+                }
+                else if (raiting == 4)
+                {
+                    realisedSales += sales * 0.7;
+                }
+                else if (raiting == 5)
+                {
+                    realisedSales += sales * 0.85;
+                }
+                else if (raiting == 6)
+                {
+                    realisedSales += sales;
+                }
+
+            }
+            Console.WriteLine($"{realisedSales:f2}");
+            Console.WriteLine($"{(double)totalRaiting / numberOfComputers:f2}");
+        }
+    }
+}
