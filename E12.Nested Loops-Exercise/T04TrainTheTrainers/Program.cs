da3d5d501c38be7a101d2e1c3c3280f6782b0531
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-18 09:51:38 +0300

    Add task TrainTheTrainers

---
+++ E12.Nested Loops-Exercise/T04TrainTheTrainers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace T04._Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfJury = int.Parse(Console.ReadLine());
+            string nameOfPresentecion = Console.ReadLine();
+
+            int countAllPresentacion = 0;
+            double sumAllGrade = 0;
+            while (nameOfPresentecion != "Finish")
+            {
+                double sumGrade = 0;
+                int currCount = 0;
+                for (int i = 1; i <= numberOfJury; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    sumGrade += grade;
+                    sumAllGrade += grade;
+                    currCount++;
+                    countAllPresentacion++;
+                }
+                Console.WriteLine($"{nameOfPresentecion} - {sumGrade / currCount:f2}.");
+                nameOfPresentecion = Console.ReadLine();
+            }
+            Console.WriteLine($"Student's final assessment is {sumAllGrade / countAllPresentacion:F2}.");
+        }
+    }
+}
