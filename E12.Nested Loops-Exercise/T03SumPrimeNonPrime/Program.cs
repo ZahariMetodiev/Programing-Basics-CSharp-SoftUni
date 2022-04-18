a094f6ff70b26e030159e73cd7697488951523a5
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-18 09:51:06 +0300

    Add task SumPrimeNonPrime

---
+++ E12.Nested Loops-Exercise/T03SumPrimeNonPrime/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace T03._Sum_Prime_Non_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+
+            int sumOfPrimeNumbers = 0;
+            int sumOfNonNumbers = 0;
+
+            while (comand != "stop")
+            {
+                int num = int.Parse(comand);
+                if (num < 0)
+                {
+                    Console.WriteLine($"Number is negative.");
+                }
+                else
+                {
+                    int count = 0;
+                    for (int i = 1; i <= num; i++)
+                    {
+                        if (num % i == 0)
+                        {
+                            count++;
+                        }
+                    }
+                    if (count == 2)
+                    {
+                        sumOfPrimeNumbers += num;
+                    }
+                    else
+                    {
+                        sumOfNonNumbers += num;
+                    }
+                    
+                }
+                comand = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {sumOfPrimeNumbers}");
+            Console.WriteLine($"Sum of all non prime numbers is: {sumOfNonNumbers}");
+        }
+    }
+}
