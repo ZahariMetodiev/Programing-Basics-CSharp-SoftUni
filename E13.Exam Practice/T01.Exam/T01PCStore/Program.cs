c0c3cbdcf48adfa684622ae84657411cd2565ea9
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-18 09:57:22 +0300

    Add task PCStore

---
+++ E13.Exam Practice/T01.Exam/T01PCStore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Т01PCStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double processorPrice = double.Parse(Console.ReadLine());
+            double videCardPrice = double.Parse(Console.ReadLine());
+            double ramMemoryPrice = double.Parse(Console.ReadLine());
+            int numberOfRamMemory = int.Parse(Console.ReadLine());
+            double percentDiscount = double.Parse(Console.ReadLine());
+
+            double convertedProcessorPrice = processorPrice * 1.57 ;
+            double convrtedVideoCardPrice = videCardPrice * 1.57;
+            double convertedRamMemotoryPrice = ramMemoryPrice * 1.57 * numberOfRamMemory;
+
+            double totalProcessorPrice = convertedProcessorPrice - (convertedProcessorPrice * percentDiscount);
+            double totalVideCardPrice = convrtedVideoCardPrice - (convrtedVideoCardPrice * percentDiscount);
+
+            double totalSum = totalProcessorPrice + totalVideCardPrice + convertedRamMemotoryPrice;
+            Console.WriteLine($"Money needed - {totalSum:f2} leva.");
+        }
+    }
+}
