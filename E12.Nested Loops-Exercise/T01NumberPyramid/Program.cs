0d2d129a8deb74ac760d9c79a55a45868135c6d5
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-18 09:49:54 +0300

    Add task NumberPyramid

---
+++ E12.Nested Loops-Exercise/T01NumberPyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T01._Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int currentNum = 1;
+
+            for (int rows = 1; rows <= num; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    Console.Write(currentNum + " ");
+                    currentNum++;
+                    if (currentNum > num)
+                    {
+                        return;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
