e4a4ba013617db3024fca35b269620f656682af2
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-19 09:55:19 +0300

    Add task Cinema

---
+++ E13.Exam Practice/E02.Exam/T04Cinema/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace T04Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hellCapacity = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            double incomeMoney = 0;
+            double ticketPrice = 5;
+            bool isFull = false;
+
+            while (input != "Movie time!")
+            {
+                int incomePeople = int.Parse(input);
+
+                if (incomePeople <= hellCapacity)
+                {
+                    hellCapacity -= incomePeople;
+                    incomeMoney += ticketPrice * incomePeople;
+                    
+                    if (incomePeople % 3 == 0)
+                    {
+                        incomeMoney -= 5;
+                    }
+                    
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    isFull = true;
+                    break;
+                }
+            }
+
+            if (isFull)
+            {
+                Console.WriteLine("The cinema is full.");
+            }
+            else
+            {
+                Console.WriteLine($"There are {hellCapacity} seats left in the cinema.");
+            }
+            Console.WriteLine($"Cinema income - {incomeMoney} lv.");
+        }
+    }
+}
