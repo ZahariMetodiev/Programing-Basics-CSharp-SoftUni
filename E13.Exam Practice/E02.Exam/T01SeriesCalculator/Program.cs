ff82fa0cc75916840680462f6ce093e6da6533ad
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-19 09:53:44 +0300

    Add task SeriesCalculator

---
+++ E13.Exam Practice/E02.Exam/T01SeriesCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T01SeriesCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serialName = Console.ReadLine();
+            int numberOfSeasons = int.Parse(Console.ReadLine());
+            int numberOfEpisode = int.Parse(Console.ReadLine());
+            double duration = double.Parse(Console.ReadLine());
+
+            double addTime = duration * 0.20;
+            double bonusTime = numberOfSeasons * 10;
+            double totalDuration = duration + addTime;
+
+            double totalTime = totalDuration * numberOfEpisode * numberOfSeasons + bonusTime;
+
+            Console.WriteLine($"Total time needed to watch the {serialName} series is {Math.Floor(totalTime)} minutes.");
+        }
+    }
+}
