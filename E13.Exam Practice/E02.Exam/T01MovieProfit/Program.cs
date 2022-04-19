314a90cf1d904007bd91dc603aa496c255aee01c
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-19 09:52:44 +0300

    Add task MovieProfit

---
+++ E13.Exam Practice/E02.Exam/T01MovieProfit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T01MovieProfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movieName = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            int tickets = int.Parse(Console.ReadLine());
+            double pricePerTicket = double.Parse(Console.ReadLine());
+            int percent = int.Parse(Console.ReadLine());
+
+            double profit = (tickets * pricePerTicket) * days;
+            double cinemaProfit = profit * percent / 100;
+            double finalProfit = profit - cinemaProfit;
+
+            Console.WriteLine($"The profit from the movie {movieName} is {finalProfit:f2} lv.");
+        }
+    }
+}
