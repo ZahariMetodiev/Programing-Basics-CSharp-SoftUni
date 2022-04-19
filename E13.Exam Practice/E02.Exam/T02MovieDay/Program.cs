11cebdf5cfc93641a460e7a053568eb0d709b28a
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-19 09:54:07 +0300

    Add task MovieDay

---
+++ E13.Exam Practice/E02.Exam/T02MovieDay/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T02MovieDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int shootingTime = int.Parse(Console.ReadLine());
+            int numberOfScenes = int.Parse(Console.ReadLine());
+            int sceneDuration = int.Parse(Console.ReadLine());
+
+            double preparation = shootingTime * 0.15;
+            double durationTime = sceneDuration * numberOfScenes + preparation;
+
+            if (shootingTime >= durationTime)
+            {
+                double timeLeft = Math.Round(shootingTime - durationTime);
+                Console.WriteLine($"You managed to finish the movie on time! You have {timeLeft} minutes left!");
+            }
+            else
+            {
+                double neededTime = durationTime - shootingTime;
+                Console.WriteLine($"Time is up! To complete the movie you need {neededTime} minutes.");
+            }
+        }
+    }
+}
