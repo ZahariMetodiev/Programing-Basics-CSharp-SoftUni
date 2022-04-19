e279ff8d056375d93cc86722a0ec884ba5cfe1d5
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-19 09:55:37 +0300

    Add task MovieStars

---
+++ E13.Exam Practice/E02.Exam/T04MovieStars/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace T04MovieStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double actorBudjet = double.Parse(Console.ReadLine());
+            string actorName = Console.ReadLine();
+
+
+            while (actorName != "ACTION")
+            {
+
+                if (actorName.Length <= 15)
+                {
+                    actorBudjet -=double.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    actorBudjet -= actorBudjet * 0.20;
+                }
+
+                if (actorBudjet <= 0)
+                {
+                    break;
+                }
+
+                actorName = Console.ReadLine();
+            }
+
+            if (actorBudjet < 0)
+            {
+                Console.WriteLine($"We need {Math.Abs(actorBudjet):f2} leva for our actors.");
+            }
+            else
+            {
+                Console.WriteLine($"We are left with {actorBudjet:f2} leva.");
+            }
+        }
+    }
+}
