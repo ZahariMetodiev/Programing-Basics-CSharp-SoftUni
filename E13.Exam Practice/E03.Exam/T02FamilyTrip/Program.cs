990f0e006b84a255a3fca3df718370602c6260e2
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-20 09:29:43 +0300

    Add task FamilyTrip

---
+++ E13.Exam Practice/E03.Exam/T02FamilyTrip/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace T02FamilyTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int numberOfNights = int.Parse(Console.ReadLine());
+            double priceForNight = double.Parse(Console.ReadLine());
+            int percentCost = int.Parse(Console.ReadLine());
+
+            if (numberOfNights > 7)
+            {
+                priceForNight *= 0.95;
+            }
+
+            double cost = budget * percentCost / 100;
+            double totalPriceForNights = numberOfNights * priceForNight;
+            double totalSum = cost + totalPriceForNights;
+
+            if (totalSum <= budget)
+            {
+                Console.WriteLine($"Ivanovi will be left with {budget - totalSum:f2} leva after vacation.");
+            }
+            else
+            {
+                Console.WriteLine($"{totalSum - budget:f2} leva needed.");
+            }
+        }
+    }
+}
