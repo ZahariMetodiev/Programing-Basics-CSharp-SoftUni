b2684432d279dc65bf3cd70432f26ee44c5ce40f
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-20 09:31:33 +0300

    Add task Renovation

---
+++ E13.Exam Practice/E03.Exam/T04Renovation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace T04Renovation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int percent = int.Parse(Console.ReadLine());
+
+            int totalArea = height * width * 4;
+            double areaForPeinting = Math.Ceiling((double)totalArea - (totalArea * percent / 100));
+
+            string command = Console.ReadLine();
+
+            while (command != "Tired!")
+            {
+                int paint = int.Parse(command);
+                areaForPeinting -= paint;
+
+                if (areaForPeinting == 0)
+                {
+                    Console.WriteLine($"All walls are painted! Great job, Pesho!");
+                    return;
+                }
+                else if (areaForPeinting <= 0)
+                {
+                    Console.WriteLine($"All walls are painted and you have {Math.Abs(areaForPeinting)} l paint left!");
+                    return;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{areaForPeinting} quadratic m left.");
+        }
+    }
+}
