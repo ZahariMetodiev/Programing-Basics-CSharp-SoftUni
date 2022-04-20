e650c341cb0298d528c8736a7fc12f53512af37e
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-20 09:29:20 +0300

    Add task PoolDay

---
+++ E13.Exam Practice/E03.Exam/T01PoolDay/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Т01PoolDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            double tax = double.Parse(Console.ReadLine());
+            double sunbed = double.Parse(Console.ReadLine());
+            double umbrella = double.Parse(Console.ReadLine());
+
+            double sunbedPrice = Math.Ceiling(people * 0.75) * sunbed;
+            double umbrellaPrice = Math.Ceiling(people / 2.00) * umbrella;
+            double taxSum = people * tax;
+
+            double total = sunbedPrice + umbrellaPrice + taxSum;
+
+            Console.WriteLine($"{total:f2} lv.");
+        }
+    }
+}
