89ba059ad46bb2bfacf0156d6205fdfdb99a84ff
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-21 09:09:34 +0300

    Add task AgencyProfit

---
+++ E13.Exam Practice/E04.Exam/T01AgencyProfit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Т01AgencyProfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string companyName = Console.ReadLine();
+            int adultTickets = int.Parse(Console.ReadLine());
+            int kidTickets = int.Parse(Console.ReadLine());
+            double adultPrice = double.Parse(Console.ReadLine());
+            double tax = double.Parse(Console.ReadLine());
+
+            double adultPriceWithTax = adultPrice + tax;
+            double kidsPriceWithTax = adultPrice * 0.30 + tax;
+
+            double totalSum = (adultTickets * adultPriceWithTax) + (kidTickets * kidsPriceWithTax);
+            double profit = totalSum * 0.20;
+            Console.WriteLine($"The profit of your agency from {companyName} tickets is {profit:f2} lv.");
+
+        }
+    }
+}
