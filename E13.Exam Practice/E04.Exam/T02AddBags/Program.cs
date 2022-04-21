87a8b46d6b955e3105b4d40472591154cb6fae5a
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-21 09:09:53 +0300

    Add task AddBags

---
+++ E13.Exam Practice/E04.Exam/T02AddBags/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace T02AddBags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double laggagePrice = double.Parse(Console.ReadLine());
+            double weight = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int numberOfLaggege = int.Parse(Console.ReadLine());
+
+            double totalLaggegePrice = 0;
+
+            if (weight < 10)
+            {
+                totalLaggegePrice = laggagePrice * 0.20;
+            }
+            else if (weight <= 20)
+            {
+                totalLaggegePrice = laggagePrice * 0.50;
+            }
+            else
+            {
+                totalLaggegePrice = laggagePrice;
+            }
+            if (days < 7)
+            {
+                totalLaggegePrice *= 1.40;
+            }
+            else if (days <= 30)
+            {
+                totalLaggegePrice *= 1.15;
+            }
+            else
+            {
+                totalLaggegePrice *= 1.10;
+            }
+
+            double totalSum = totalLaggegePrice * numberOfLaggege;
+            Console.WriteLine($"The total price of bags is: {totalSum:f2} lv. ");
+        }
+    }
+}
