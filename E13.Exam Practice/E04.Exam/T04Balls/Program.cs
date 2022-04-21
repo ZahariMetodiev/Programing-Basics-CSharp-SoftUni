1432e72a22871e79107601e64ccd5c60f7555172
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-21 09:10:54 +0300

    Add task Balls

---
+++ E13.Exam Practice/E04.Exam/T04Balls/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace T04Balls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ballNumber = int.Parse(Console.ReadLine());
+
+            double points = 0;
+            int redCount = 0;
+            int orangeCount = 0;
+            int yellowCount = 0;
+            int whiteCount = 0;
+            int blackCount = 0;
+            int otherCount = 0;
+
+            for (int i = 1; i <= ballNumber; i++)
+            {
+                string color = Console.ReadLine();
+
+                switch (color)
+                {
+                    case "red":
+                        points += 5;
+                        redCount++;
+                        break;
+                    case "orange":
+                        points += 10;
+                        orangeCount++;
+                        break;
+                    case "yellow":
+                        points += 15;
+                        yellowCount++;
+                        break;
+                    case "white":
+                        points += 20;
+                        whiteCount++;
+                        break;
+                    case "black":
+                        points = Math.Floor(points / 2);
+                        blackCount++;
+                        break;
+                    default:
+                        otherCount++;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Total points: {points}");
+            Console.WriteLine($"Red balls: {redCount}");
+            Console.WriteLine($"Orange balls: {orangeCount}");
+            Console.WriteLine($"Yellow balls: {yellowCount}");
+            Console.WriteLine($"White balls: {whiteCount}");
+            Console.WriteLine($"Other colors picked: {otherCount}");
+            Console.WriteLine($"Divides from black balls: {blackCount}");
+        }
+    }
+}
