0c9eb3658daba586bb1583f515e956fda045281d
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-22 09:17:29 +0300

    Add task CinemaVoucher

---
+++ E13.Exam Practice/E05.Exam/T04CinemaVoucher/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace T04CinemaVoucher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vaucherAmount = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            int ticketCounter = 0;
+            int anotherCounter = 0;
+            int sum = 0;
+
+            while (command != "End")
+            {
+                if (command.Length > 8)
+                {
+                    sum += command[0] + command[1];
+                    if (sum > vaucherAmount)
+                    {
+                        break;
+                    }
+                    ticketCounter++;
+                }
+                else
+                {
+                    sum += command[0];
+                    if (sum > vaucherAmount)
+                    {
+                        break;
+                    }
+                    anotherCounter++;
+                }
+                
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(ticketCounter);
+            Console.WriteLine(anotherCounter);
+        }
+    }
+}
