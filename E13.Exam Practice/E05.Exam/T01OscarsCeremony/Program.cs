3890e237a5d4a1a6dcee8fd772ea6e55bf8a0c37
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-22 09:15:41 +0300

    Add task OscarsCeremony

---
+++ E13.Exam Practice/E05.Exam/T01OscarsCeremony/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T01OscarsCeremony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hellRent = int.Parse(Console.ReadLine());
+
+            double statuettesPrice = hellRent * 0.70;
+            double cateringPrice = statuettesPrice * 0.85;
+            double soundSystem = cateringPrice / 2;
+
+            double totalSum = hellRent + statuettesPrice + cateringPrice + soundSystem;
+
+            Console.WriteLine($"{totalSum:f2}");
+        }
+    }
+}
