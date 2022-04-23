c89bfa61a6ce6b16a00a117a8e3a3fec96783827
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-23 09:10:21 +0300

    Add task FoodForPets

---
+++ E13.Exam Practice/E06.Exam/T04FoodForPets/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace T04FoodForPets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double foodAmount = double.Parse(Console.ReadLine());
+
+            double totalEatenFromDog = 0;
+            double totalEatenFromCat = 0;
+            double totalFoodEaten = 0;
+            double totalBiscuits = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                double totalFoodPerDay = 0;
+                double eatenFromDog = double.Parse(Console.ReadLine());
+                double eatenFromCat = double.Parse(Console.ReadLine());
+
+                totalEatenFromCat += eatenFromCat;
+                totalEatenFromDog += eatenFromDog;
+                totalFoodPerDay = eatenFromCat + eatenFromDog;
+
+                if (i % 3 == 0)
+                {
+                    totalBiscuits += totalFoodPerDay * 0.10; 
+                }
+                totalFoodEaten += totalFoodPerDay;
+            }
+
+            double percentFoodEaten = totalFoodEaten / foodAmount * 100;
+            double percentDogEaten = totalEatenFromDog / totalFoodEaten * 100;
+            double percentCatEaten = totalEatenFromCat / totalFoodEaten * 100;
+
+            Console.WriteLine($"Total eaten biscuits: {Math.Round(totalBiscuits)}gr.");
+            Console.WriteLine($"{percentFoodEaten:F2}% of the food has been eaten.");
+            Console.WriteLine($"{percentDogEaten:f2}% eaten from the dog.");
+            Console.WriteLine($"{percentCatEaten:f2}% eaten from the cat.");
+        }
+    }
+}
