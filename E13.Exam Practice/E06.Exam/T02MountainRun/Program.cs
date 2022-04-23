dc22b418d8e1c7a33e659204c69b5d0dc5cbae9b
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-23 09:09:11 +0300

    Add task MountainRun

---
+++ E13.Exam Practice/E06.Exam/T02MountainRun/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T02MountainRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timeForOneMeter = double.Parse(Console.ReadLine());
+
+            double time = timeForOneMeter * distance;
+            double delayTime = Math.Floor( distance / 50) * 30;
+            double totalTime = time + delayTime;
+
+            if (totalTime < record)
+            {
+                Console.WriteLine($" Yes! The new record is {totalTime:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No! He was {totalTime - record:F2} seconds slower.");
+            }
+        }
+    }
+}
