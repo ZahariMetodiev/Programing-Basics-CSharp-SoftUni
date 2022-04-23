961547983179c50741f78d445cd55bdb8728af3e
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-23 09:09:34 +0300

    Add task EnergyBooster

---
+++ E13.Exam Practice/E06.Exam/T03EnergyBooster/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace T03EnergyBooster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string size = Console.ReadLine();
+            int numberOfSet = int.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            if (size == "small")
+            {
+                switch (fruit)
+                {
+                    case "Watermelon":
+                        price = 56 * 2;
+                        break;
+                    case "Mango":
+                        price = 36.66 * 2;
+                    break;
+                    case "Pineapple":
+                        price = 42.10 * 2;
+                        break;
+                    case "Raspberry":
+                        price = 20.00 * 2;
+                        break;
+                }
+            }
+            else if (size == "big")
+            {
+                switch (fruit)
+                {
+                    case "Watermelon":
+                        price = 28.70 * 5;
+                        break;
+                    case "Mango":
+                        price = 19.60 * 5;
+                    break;
+                    case "Pineapple":
+                        price = 24.80 * 5;
+                        break;
+                    case "Raspberry":
+                        price = 15.20 * 5;
+                        break;
+                }
+            }
+            double sum = price * numberOfSet;
+
+            if (sum >= 400 && sum <= 1000)
+            {
+                sum *= 0.85;
+            }
+            else if (sum > 1000)
+            {
+                sum *= 0.50;
+            }
+            Console.WriteLine($"{sum:f2} lv.");
+        }
+    }
+}
