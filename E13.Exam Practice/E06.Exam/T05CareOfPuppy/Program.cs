514da87a56d4f02302b4a1cf283ef382ed9e4a0e
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-23 09:11:08 +0300

    Add task CareOfPuppy

---
+++ E13.Exam Practice/E06.Exam/T05CareOfPuppy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace T05CareOfPuppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodAmount = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            int convertedFood = foodAmount * 1000;
+            int totalFood = 0;
+
+            while (command != "Adopted")
+            {
+                int currentFood = int.Parse(command);
+                totalFood += currentFood;
+                command = Console.ReadLine();
+            }
+
+            if (totalFood <= convertedFood)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {convertedFood - totalFood} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {totalFood - convertedFood} grams more.");
+            }
+        }
+    }
+}
