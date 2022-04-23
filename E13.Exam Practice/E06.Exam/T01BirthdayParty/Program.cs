e8900915ce43714578604457d33d1e369a3ac114
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-23 09:07:55 +0300

    Add task BirthdayParty

---
+++ E13.Exam Practice/E06.Exam/T01BirthdayParty/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T01BirthdayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hellRent = double.Parse(Console.ReadLine());
+
+            double cake = hellRent * 0.20;
+            double drinks = cake * 0.55;
+            double animator = hellRent / 3;
+
+            double neededBudjet = hellRent + cake + drinks + animator;
+
+            Console.WriteLine(neededBudjet);
+        }
+    }
+}
