fcc3ca4a00e6670c0ec8eda03539b912bf06f5bd
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-23 09:08:46 +0300

    Add task CatWalking

---
+++ E13.Exam Practice/E06.Exam/T02CatWalking/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T02CatWalking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesWalk = int.Parse(Console.ReadLine());
+            int numberOfWalk = int.Parse(Console.ReadLine());
+            double calories = double.Parse(Console.ReadLine());
+
+            int totalMinutesForWalk = minutesWalk * numberOfWalk;
+            double totalCalories = totalMinutesForWalk * 5;
+            double neededCalories = calories / 2;
+
+            if (totalCalories >= neededCalories)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {totalCalories}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {totalCalories}.");
+            }
+        }
+    }
+}
