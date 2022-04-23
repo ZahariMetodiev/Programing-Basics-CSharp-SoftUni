20f71a4cdf3a48e4fc3182ed23f69b1d68e7c9d5
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-23 09:08:24 +0300

    Add task ChangeBureau

---
+++ E13.Exam Practice/E06.Exam/T01ChangeBureau/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T01ChangeBureau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBitcoin = int.Parse(Console.ReadLine());
+            double numberOfChineseYuan = double.Parse(Console.ReadLine());
+            double commision = double.Parse(Console.ReadLine());
+
+            int bitcoinSum = numberOfBitcoin * 1168;
+            double dollarSum = numberOfChineseYuan * 0.15;
+            double convertedDollar = dollarSum * 1.76;
+
+            double sumWithoutCommision = (bitcoinSum + convertedDollar) / 1.95;
+            double totalCommision = sumWithoutCommision * commision / 100;
+            double totalSum = sumWithoutCommision - totalCommision;
+
+            Console.WriteLine($"{totalSum:f2}");
+        }
+    }
+}
